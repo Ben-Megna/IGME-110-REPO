37243207573be4bbd4968eb4bc84b1c896f4438c
Ben-Megna <bpm7652@rit.edu>
2024-09-03 20:14:42 -0400

    Create BenMegna-IGME-110-My-Repo.js

---
+++ BenMegna-IGME-110-My-Repo.js
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Random Monarchs Listicle</title>
+</head>
+<body>
+  <h1>Random Monarchs Listicle</h1>
+  <p>Source: <a href="https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://www.ranker.com/list/notable-monarch_s)/reference&ved=2ahUKEwj70KrpmpaIAxVPE1kFHYppM38QFnoECBcQAQ&usg=AOvVaw1ubyv4flhiarf5H7yGfgdD">Ranker</a></p>
+  
+  <h2>German Rulers:</h2>
+  <ul>
+    <li>
+      <a href="https://en.wikipedia.org/wiki/Wilhelm_II">Kaiser Wilhelm II</a>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/6/6e/Kaiser_Wilhelm_II_of_Germany_-_1902.jpg" alt="Wilhelm II">
+    </li>
+    <li>
+      <a href="https://en.wikipedia.org/wiki/Otto_the_Great">Otto The Great</a>
+      <img src="https://theriagames.com/wp-content/uploads/2023/10/otto-the-great.jpg" alt="Otto">
+    </li>
+    <li>
+      <a href="https://en.wikipedia.org/wiki/Ferdinand_II,_Holy_Roman_Emperor">Ferdinand II</a>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Ferdinand_II%2C_Holy_Roman_Emperor_%28Giovanni_Pietro_de_Pomis%2C_1619%29.jpg/433px-Ferdinand_II%2C_Holy_Roman_Emperor_%28Giovanni_Pietro_de_Pomis%2C_1619%29.jpg" alt="Ferdinand II">
+    </li>
+  </ul>
+
+  <h2>French Rulers:</h2>
+  <ul>
+    <li>
+      <a href="https://en.wikipedia.org/wiki/Charlemagne">Emperor Charlemagne</a>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/9/98/Louis-F%C3%A9lix_Amiel_-_Charlemagne_empereur_d'Occident_%28742-814%29.jpg" alt="Charlemagne">
+    </li>
+    <li>
+      <a href="https://en.wikipedia.org/wiki/Louis_XVI">King Louis XVI</a>
+      <img src="https://hips.hearstapps.com/hmg-prod/images/gettyimages-167889617.jpg" alt="Louis XVI">
+    </li>
+  </ul>
+</body>
+</html>
